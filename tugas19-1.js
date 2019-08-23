71527efc42900dddeffeaf9b13159b32c0610b4e
Aria Bagas <aariabagass@gmail.com>
2019-08-23 22:15:55 +0700

    JS Task 19
    string repeat

---
+++ tugas19-1.js
@@ -0,0 +1,6 @@
+var repeater = () => {
+  spirit = 'Semangat...'
+  console.log(spirit.repeat(5));
+  return repeater;
+}
+repeater()
---
+++ tugas19-2.js
@@ -0,0 +1,13 @@
+var repeater = () => {
+  console.log("*".repeat(5));
+  console.log("*".repeat(25));
+  console.log("*".repeat(45));
+  console.log("*".repeat(65));
+  console.log("*".repeat(95));
+  console.log("*".repeat(65));
+  console.log("*".repeat(45));
+  console.log("*".repeat(25));
+  console.log("*".repeat(5));
+  return repeater;
+}
+repeater()
